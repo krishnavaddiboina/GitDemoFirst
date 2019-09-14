9123032a2e1a479efd6f3771b2d37dc57a8d7204
krish <krish@LAPTOP-V0UFI2G3>
2019-09-14 13:04:37 +0530

    added message as requested

--- src/com/krishna/demo/Test1.java
+++ src/com/krishna/demo/Test1.java
@@ -2,6 +2,6 @@ package com.krishna.demo;
 
 public class Test1 {
 	public static void main(String[] args) {
-		System.out.println("Testing from developer side");
+		System.out.println("Testing from developer side and changed message");
 	}
 }
